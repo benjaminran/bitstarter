b9fba09419dacc409b9b0e0f98d928abd5e33412
Ubuntu <ubuntu@ip-172-31-39-20.us-west-2.compute.internal>
2013-08-18 20:00:48 +0000

    new index.html and hebang added o web.js

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
-var express = require('express');
+#!usr/bin/node 
 
-var app = express.createServer(express.logger());
+var express = require('express');
+var app = express();
+app.use(express.logger());
 
 var fs = require('fs');
 
@@ -8,7 +10,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('./index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
